65c64a7bdf46c5a81c10f591256d62dec278d8a8
Sunskimmer82 <92886026+Sunskimmer822@users.noreply.github.com>
2022-10-23 19:56:54 -0500

    abandoning picktool for a bit

--- src/main/java/com/serenas/qolmod/QOLModClient.java
+++ src/main/java/com/serenas/qolmod/QOLModClient.java
@@ -8,18 +8,39 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
+import com.serenas.qolmod.QOLMod;
+
 public class QOLModClient implements ClientModInitializer {
     
-    private static KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.qolmod.pickTool",InputUtil.Type.MOUSE,GLFW.GLFW_MOUSE_BUTTON_5,"category.qolmod.QoL")); 
-    
+    private static KeyBinding pickToolBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.qolmod.pickTool",InputUtil.Type.MOUSE,GLFW.GLFW_MOUSE_BUTTON_5,"category.qolmod.QoL")); 
+    private static KeyBinding align0deg = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.qolmod.alignToZeroDegrees",InputUtil.Type.KEYSYM,GLFW.GLFW_KEY_G,"category.qolmod.QoL")); 
+    private static KeyBinding align90deg = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.qolmod.alignToNinetyDegrees",InputUtil.Type.KEYSYM,GLFW.GLFW_KEY_H,"category.qolmod.QoL")); 
+    private static KeyBinding align180deg = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.qolmod.alignToHundredEightyDegrees",InputUtil.Type.KEYSYM,GLFW.GLFW_KEY_V,"category.qolmod.QoL")); 
+    private static KeyBinding align270deg = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.qolmod.alignToTwoSeventyDegrees",InputUtil.Type.KEYSYM,GLFW.GLFW_KEY_B,"category.qolmod.QoL")); 
     
     @Override
     public void onInitializeClient() {
 
         
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-        while (keyBinding.wasPressed()) {
+        while (pickToolBind.wasPressed()) {
             
+        }
+        while (align0deg.wasPressed()) {
+            client.player.setYaw(0f);
+            QOLMod.LOGGER.info("Aligned to 0 degrees");
+        }
+        while (align90deg.wasPressed()) {
+            client.player.setYaw(90f);
+            QOLMod.LOGGER.info("Aligned to 90 degrees");
+        }
+        while (align180deg.wasPressed()) {
+            client.player.setYaw(180f);
+            QOLMod.LOGGER.info("Aligned to 180 degrees");
+        }
+        while (align270deg.wasPressed()) {
+            client.player.setYaw(270f);
+            QOLMod.LOGGER.info("Aligned to 0 degrees");
         }
         });
     }
